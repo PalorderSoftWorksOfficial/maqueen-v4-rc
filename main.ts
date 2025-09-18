7b9a4014cfe6d1aab503114bbb2b1a0237df227c
PalorderSoftWorksOfficial <palordersoftworks@gmail.com>
2025-09-18 11:10:03 +0200

    main.blocks, main.ts bijwerken

--- main.ts
+++ main.ts
@@ -1,22 +1,28 @@
-// Typescript syntax is ass
-radio.onReceivedString(function (name) {
-    if (name == "forward") {
+radio.onReceivedString(function (msg) {
+    if (msg == "forward") {
         maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 255)
-    } else if (name == "left") {
-        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 255)
-        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CCW, 255)
-    } else if (name == "right") {
-        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CCW, 255)
-        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 255)
-    } else if (name == "stop") {
+    } else if (msg == "left") {
+        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 50)
+        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CCW, 50)
+    } else if (msg == "right") {
+        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CCW, 50)
+        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 50)
+    } else if (msg == "stop") {
+        maqueen.motorStop(maqueen.Motors.All)
+    } else if (msg.substr(0, 4) == "msg:") {
+        basic.showString(msg.substr(4))
+    } else if (msg == "battery") {
+        radio.sendNumber(Maqueen_V5.getBatteryData(Maqueen_V5.BatteryType.Alkaline))
+    } else if (msg == "P1") {
+        basic.pause(1000)
+        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 255)
+        basic.pause(1500)
+        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 50)
+        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CCW, 50)
+        basic.pause(500)
+        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 255)
+        basic.pause(1000)
         maqueen.motorStop(maqueen.Motors.All)
     }
 })
 radio.setGroup(1)
-basic.forever(function () {
-    radio.onReceivedString(function(msg:string) {
-    if (msg.substr(0, 4) == "msg:") {
-        basic.showString(msg.substr(4))
-    }
-})
-})
