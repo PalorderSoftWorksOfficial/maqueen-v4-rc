b3ff150ce95243ba31e12ce3748ddd98632598b9
PalorderSoftWorksOfficial <palordersoftworks@gmail.com>
2025-09-18 09:29:02 +0200

    main.blocks, main.ts bijwerken

--- main.ts
+++ main.ts
@@ -1,24 +1,22 @@
 // Typescript syntax is ass
 radio.onReceivedString(function (name) {
     if (name == "forward") {
-        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 50)
-        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 50)
+        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 255)
     } else if (name == "left") {
-        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 50)
-        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CCW, 50)
+        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 255)
+        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CCW, 255)
     } else if (name == "right") {
-        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CCW, 50)
-        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 50)
+        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CCW, 255)
+        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 255)
     } else if (name == "stop") {
         maqueen.motorStop(maqueen.Motors.All)
     }
 })
-radio.onReceivedString(function(msg:string) {
+radio.setGroup(1)
+basic.forever(function () {
+    radio.onReceivedString(function(msg:string) {
     if (msg.substr(0, 4) == "msg:") {
         basic.showString(msg.substr(4))
     }
 })
-radio.setGroup(0)
-basic.forever(function () {
-	
 })
