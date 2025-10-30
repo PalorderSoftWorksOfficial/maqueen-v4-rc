ebb2f9aad1f313967bb98016f96bbd8b619512a9
PalorderSoftWorksOfficial <palordersoftworks@gmail.com>
2025-10-30 09:24:55 +0100

    pxt.json, main.blocks, main.ts bijwerken

--- main.ts
+++ main.ts
@@ -2,12 +2,17 @@
 radio.onReceivedString(function (msg) {
     if (msg == "forward") {
         maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 255)
+        datalogger.log(datalogger.createCV("forward", 255))
     } else if (msg == "left") {
         maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 50)
         maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CCW, 50)
+        basic.pause(200)
+        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 255)
     } else if (msg == "right") {
         maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CCW, 50)
         maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 50)
+        basic.pause(200)
+        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 255)
     } else if (msg == "stop") {
         maqueen.motorStop(maqueen.Motors.All)
     } else if (msg.substr(0, 4) == "msg:") {
@@ -24,6 +29,22 @@ radio.onReceivedString(function (msg) {
         maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 255)
         basic.pause(1000)
         maqueen.motorStop(maqueen.Motors.All)
+    } else if (msg == "P2") {
+        basic.pause(1000)
+        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 255)
+        basic.pause(1500)
+        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 50)
+        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CCW, 50)
+        basic.pause(500)
+        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 255)
+        basic.pause(1000)
+        maqueen.motorStop(maqueen.Motors.All)
+    } else if (msg == "backwards") {
+        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CCW, 255)
+        datalogger.log(datalogger.createCV("forward", -255))
+        datalogger.log(datalogger.createCV("backwards", 255))
     }
 })
 radio.setGroup(1)
+datalogger.mirrorToSerial(true)
+serial.writeString("initiated!")
